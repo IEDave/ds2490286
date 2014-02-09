dde6143c32dcf320e9bf9f3b32dfdec787c3efb4
IEDave <dwsmit@dslextreme.com>
2014-02-09 11:47:34 -0800

    Signed-off-by: IEDave <dwsmit@dslextreme.com>

--- Hangman/main.cpp
+++ Hangman/main.cpp
@@ -18,20 +18,21 @@
 using namespace std;
 
 //Global Constants
-const int WRD_COL = 32, DISP_COL = 15, DISP_ROW = 15, WRD_ROW = 10,
-          MAX_WORD = 10;
+const int WRD_COL = 32, DISP_COL = 15;
 
 //Function Prototypes
 void upCase(char &);
 void iniWord(char [][WRD_COL]);
 void iniDisp(char [][DISP_COL]);
-void doHang(char [][DISP_COL],char [][WRD_COL]);
-void dspHang(char [][DISP_COL]);
-void updtHng(char [][DISP_COL]);
+void doHang(char [][DISP_COL],char [][WRD_COL], int, int, int);
+void dspHang(char [][DISP_COL],int);
+void updtHng(char [][DISP_COL],unsigned short);
 bool strFind(char [], char, unsigned short, char [][DISP_COL]);
 
 //Execution begins here
 int main(int argc, char** argv) {
+    //Define local constants
+    const int DISP_ROW = 15, WRD_ROW = 10, MAX_WORD = 10;
     //Define local variables
     char wrdList[WRD_ROW][WRD_COL], sDisp[DISP_ROW][DISP_COL], iCont;
     //Seed random number generator
@@ -41,8 +42,8 @@ int main(int argc, char** argv) {
     iniDisp(sDisp);
     //Main execution loop
     do {
-        //call function doHang(wrdList,sDisp) - play 1 game of Hangman
-        doHang(sDisp,wrdList);
+        //call function doHang() - play 1 game of Hangman
+        doHang(sDisp,wrdList,DISP_ROW,WRD_ROW,MAX_WORD);
         //Confirm with user - are we done?
         cout << "Play again(Y/N)?";
         cin >> iCont;
@@ -90,16 +91,21 @@ void iniDisp(char dMatrix[][DISP_COL]) {
     return;
 }
 
-void doHang(char dMatrix[][DISP_COL],char words[][WRD_COL]) {
+void doHang(char dMatrix[][DISP_COL],char words[][WRD_COL],
+            int rows, int cols, int totWrds) {
     //define local variables
     char word[WRD_COL],letr;
-    unsigned short badLetr = 0, wrdLen,wrdLoc;
+    unsigned short badLetr = 0,wrdLen,wrdLoc;
     bool isWord;
     //randomly pick a word, get its length & location in array
-    //wrdLoc = rand()%MAX_WORD;
-    wrdLoc = 0;
+    wrdLoc = rand()%totWrds;
     strcpy(word,words[wrdLoc]);
     wrdLen = strlen(word);
+    //Initialize display matrix, print cheery welcome message & display
+    //empty gallows
+    iniDisp(dMatrix);
+    cout << "Welcome to HANGMAN" << endl;
+    dspHang(dMatrix,rows);
     //loop
     do {
         //ask user for a letter (cvt. to uppercase)
@@ -108,33 +114,33 @@ void doHang(char dMatrix[][DISP_COL],char words[][WRD_COL]) {
         upCase(letr);
         if (letr >= 'A' && letr <= 'Z') {
             if (strFind(word, letr, wrdLen, dMatrix)) {
-                dspHang(dMatrix);
+                dspHang(dMatrix,rows);
                 cout << "Found " << letr << "." << endl;
             } else {
                 badLetr++;
-                updtHng(dMatrix);
-                dspHang(dMatrix);
+                updtHng(dMatrix,badLetr);
+                dspHang(dMatrix,rows);
                 cout << "Didn't find " << letr << "." << endl;
             }
         } else {
               badLetr++;
-              updtHng(dMatrix);
-              dspHang(dMatrix);
+              updtHng(dMatrix,badLetr);
+              dspHang(dMatrix,rows);
               cout << "Didn't find " << letr << "." << endl;
         }
         isWord = (!strncmp(word,dMatrix[11],wrdLen));
-    } while ((badLetr < MAX_WORD-1) && (!isWord));
+    } while ((badLetr < totWrds-1) && (!isWord));
     if (isWord) {
         cout << "YOU WIN!";
-    } else if (badLetr >= MAX_WORD-1) {
+    } else if (badLetr >= totWrds-1) {
         cout << "YOU LOSE!";
     }
     cout << endl;
     return;
 }
 
-void dspHang(char dMatrix[][DISP_COL]) {
-    for (int row = 0; row <= DISP_ROW-1; row++) {
+void dspHang(char dMatrix[][DISP_COL], int rows) {
+    for (int row = 0; row <= rows-1; row++) {
         for (int col = 0; col <= DISP_COL-1; col++) {
             if (dMatrix[row][col] != '\0') 
                 cout << dMatrix[row][col];
@@ -149,22 +155,31 @@ void dspHang(char dMatrix[][DISP_COL]) {
 void updtHng(char dMatrix[][DISP_COL],unsigned short errCnt) {
     switch(errCnt) {
         case 1:
+            dMatrix[3][9] = 'O';
             break;
         case 2:
+            dMatrix[4][9] = '|';
             break;
         case 3:
+            dMatrix[4][8] = static_cast<char>(92);
             break;
         case 4:
+            dMatrix[4][10] = '/';
             break;
         case 5:
+            dMatrix[5][9] = '|';
             break;
         case 6:
+            dMatrix[6][8] = '/';
             break;
         case 7:
+            dMatrix[6][10] = static_cast<char>(92);
             break;
         case 8:
+            dMatrix[7][7] = '-';
             break;
         case 9:
+            dMatrix[7][11] = '-';
             break;      
     }
     return;
